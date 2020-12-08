3773656aaf52a9bb8245b12154214b4937d5e514
Alberto Guzman <albertogx@icloud.com>
2020-12-07 23:19:35 -0500

    part 2 react course

---
+++ pics/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const index = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
--- season/src/index.js
+++ season/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner';
+import Clock from './Clock';
 class App extends React.Component {
-    constructor(props){
-        super(props);
-        // this is the only time we do direct assignment to
-        // to this.state
-        this.state = { lat: null, errorMessage: '' };
+    // constructor(props){
+    //     super(props);
+    //     // this is the only time we do direct assignment to
+    //     // to this.state
+    //     this.state = { lat: null, errorMessage: '' };
+        
+    // }
+    // above code equal but longer
+    state = { lat: null, errorMessage: '' };
 
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position =>  this.setState({lat: position.coords.latitude}),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+    /* above code equivalent but shorter
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             position =>  {
                 // we called setstate, setstate is a function inside
@@ -16,29 +30,34 @@ class App extends React.Component {
             },
             // err => console.log(err)
             err => {
-                this.setState({ errorMessage: err.message })
+                this.setState({ errorMessage: err.message });
             }
         );
     }
+    */
 
-    componentDidMount(){
-        console.log('My component was rendered to the screen');
+    renderContent () {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>;
+        }         
+        if (!this.state.errorMessage && this.state.lat){
+            //return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner message="Please accept location request"/>;
     }
-    
     componentDidUpdate(){
         console.log('My component was just updated - it rerendered!');
     }
     // React needs the render() to work with class
-    render() {        
-        
-            if(this.state.errorMessage && !this.state.lat){
-                return <div>Error: {this.state.errorMessage}</div>
-            }         
-            if (!this.state.errorMessage && this.state.lat){
-                return <div>Latitude: {this.state.lat}</div>
-            }
-
-            return <div>Loading!</div>;
+    render() { 
+        return (
+        <div>
+            {this.renderContent()}
+            <Clock />
+        </div>
+        );       
 }
 } 
 // <div>
